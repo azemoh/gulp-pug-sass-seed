b331c412db890b127d9f8da4e8b134e1f43475a1
azemoh <buzzme46@gmail.com>
2015-10-02 09:13:30 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Jade-Sass Starter project.
+
+This is a Jade and Sass starter project using gulp for task automation.
+
+This project uses...
+
+1. [browser-sync](https://github.com/browsersync/browser-sync) to launch a local server and do live reloads as sass and jade files changes
+2. [gulp-jade](https://github.com/phated/gulp-jade) to compile jade files.
+3. [gulp-data](https://github.com/colynb/gulp-data) to pass data to jade. this project uses JSON as the data source, however you can generate data objects from a variety of sources: json, front-matter, database, etc... see gulp-data [README](https://github.com/colynb/gulp-data)
+4. [gulp-sass](https://github.com/dlmanning/gulp-sass) to compile sass files.
+5. [gulp-autoprefixer](https://github.com/sindresorhus/gulp-autoprefixer) to add vendor prefixes to css files
+
+### To run
+- Execute `npm install` from this directory to install dependencies.
+- Execute `gulp` to run all tasks, launch the browser sync local server and watch for changes.
+
---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+/*global require*/
+"use strict";
+
+var gulp = require('gulp'),
+	path = require('path'),
+	data = require('gulp-data'),
+	jade = require('gulp-jade'),
+	prefix = require('gulp-autoprefixer'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync');
+
+/*
+* Change directories here
+*/
+var settings = {
+	publicDir: '_site',
+	sassDir: 'assets/css',
+	cssDir: '_site/assets/css'
+};
+
+/**
+ * Compile .jade files and pass in data from json file
+ * matching file name. index.jade - index.jade.json
+ */
+gulp.task('jade', function () {
+	return gulp.src('*.jade')
+		.pipe(data(function (file) {
+			return require('./_data/' + path.basename(file.path) + '.json');
+		}))
+		.pipe(jade())
+		.pipe(gulp.dest(settings.publicDir));
+});
+
+/**
+ * Recompile .jade files and live reload the browser
+ */
+gulp.task('jade-rebuild', ['jade'], function () {
+	browserSync.reload();
+});
+
+/**
+ * Wait for jade and sass tasks, then launch the browser-sync Server
+ */
+gulp.task('browser-sync', ['sass', 'jade'], function () {
+	browserSync({
+		server: {
+			baseDir: settings.publicDir
+		},
+		notify: false
+	});
+});
+
+/**
+ * Compile .scss files into public css directory With autoprefixer no
+ * need for vendor prefixes then live reload the browser.
+ */
+gulp.task('sass', function () {
+	return gulp.src(settings.sassDir + '/*.scss')
+		.pipe(sass({
+			includePaths: [settings.sassDir],
+			outputStyle: 'compressed',
+			onError: browserSync.notify
+		}))
+		.pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
+		.pipe(gulp.dest(settings.cssDir))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch .jade files run jade-rebuild then reload BrowserSync
+ */
+gulp.task('watch', function () {
+	gulp.watch(settings.sassDir + '/**', ['sass']);
+	gulp.watch(['*.jade', '**/*.jade'], ['jade-rebuild']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync then watch
+ * files for changes
+ */
+gulp.task('default', ['browser-sync', 'watch']);
