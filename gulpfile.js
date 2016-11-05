5a239588f59f5737744247ca52e409a78547032e
Joshua Azemoh <joshazemoh@gmail.com>
2016-11-05 11:52:53 +0100

    change folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,21 @@
 "use strict";
 
 var gulp = require('gulp'),
-	path = require('path'),
-	data = require('gulp-data'),
-	pug = require('gulp-pug'),
-	prefix = require('gulp-autoprefixer'),
-	sass = require('gulp-sass'),
-	browserSync = require('browser-sync');
+  path = require('path'),
+  data = require('gulp-data'),
+  pug = require('gulp-pug'),
+  prefix = require('gulp-autoprefixer'),
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync');
 
 /*
-* Change directories here
-*/
-var settings = {
-	publicDir: '_site',
-	sassDir: 'assets/css',
-	cssDir: '_site/assets/css'
+ * Directories here
+ */
+var paths = {
+  public: './public/',
+  sass: './src/sass/',
+  css: './public/css/',
+  data: './src/_data/'
 };
 
 /**
@@ -23,31 +24,31 @@ var settings = {
  * matching file name. index.pug - index.pug.json
  */
 gulp.task('pug', function () {
-	return gulp.src('*.pug')
-		.pipe(data(function (file) {
-			return require('./_data/' + path.basename(file.path) + '.json');
-		}))
-		.pipe(pug())
-		.pipe(gulp.dest(settings.publicDir));
+  return gulp.src('./src/*.pug')
+    .pipe(data(function (file) {
+      return require(paths.data + path.basename(file.path) + '.json');
+    }))
+    .pipe(pug())
+    .pipe(gulp.dest(paths.public));
 });
 
 /**
  * Recompile .pug files and live reload the browser
  */
-gulp.task('pug-rebuild', ['pug'], function () {
-	browserSync.reload();
+gulp.task('rebuild', ['pug'], function () {
+  browserSync.reload();
 });
 
 /**
  * Wait for pug and sass tasks, then launch the browser-sync Server
  */
 gulp.task('browser-sync', ['sass', 'pug'], function () {
-	browserSync({
-		server: {
-			baseDir: settings.publicDir
-		},
-		notify: false
-	});
+  browserSync({
+    server: {
+      baseDir: paths.public
+    },
+    notify: false
+  });
 });
 
 /**
@@ -55,15 +56,19 @@ gulp.task('browser-sync', ['sass', 'pug'], function () {
  * need for vendor prefixes then live reload the browser.
  */
 gulp.task('sass', function () {
-	return gulp.src(settings.sassDir + '/*.scss')
-		.pipe(sass({
-			includePaths: [settings.sassDir],
-			outputStyle: 'compressed'
-		}))
-		.on('error', sass.logError)
-		.pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
-		.pipe(gulp.dest(settings.cssDir))
-		.pipe(browserSync.reload({stream: true}));
+  return gulp.src(paths.sass + '*.scss')
+    .pipe(sass({
+      includePaths: [paths.sass],
+      outputStyle: 'compressed'
+    }))
+    .on('error', sass.logError)
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+      cascade: true
+    }))
+    .pipe(gulp.dest(paths.css))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 /**
@@ -71,9 +76,12 @@ gulp.task('sass', function () {
  * Watch .pug files run pug-rebuild then reload BrowserSync
  */
 gulp.task('watch', function () {
-	gulp.watch(settings.sassDir + '/**', ['sass']);
-	gulp.watch(['*.pug', '**/*.pug'], ['pug-rebuild']);
+  gulp.watch(paths.sass + '**/*.scss', ['sass']);
+  gulp.watch('./src/**/*.pug', ['rebuild']);
 });
+
+// Build task compile sass and pug.
+gulp.task('build', ['sass', 'pug']);
 
 /**
  * Default task, running just `gulp` will compile the sass,
